b04e4c457dc879d3edf8475440cd87adba4fd5b6
poo <poo@email.com>
2021-02-26 16:46:56 +0200

    remove pip sudo

--- app_script.sh
+++ app_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sudo -S apt update
 sudo -S apt install -y python3-pip
-python3 -m pip install flask
+sudo -H python3 -m pip install flask
 sudo wget -O /home/app.py https://raw.githubusercontent.com/poolfire/pythontestproj/master/app.py
 sudo wget -O /etc/systemd/system/flaskapp.service https://raw.githubusercontent.com/poolfire/pythontestproj/master/flaskapp.service
 sudo systemctl daemon-reload
