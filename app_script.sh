7593b723a66da580e9fab4f09d7b68780e93d943
poo <poo@email.com>
2021-02-26 14:14:42 +0200

    typo with wget

--- app_script.sh
+++ app_script.sh
@@ -3,7 +3,7 @@ sudo -S apt update
 sudo apt install -y python3-pip
 sudo python3 -m pip install flask
 sudo wget -O /home/app.py https://raw.githubusercontent.com/poolfire/pythontestproj/master/app.py
-sudo wget -0 /etc/systemd/system/flaskapp.service https://raw.githubusercontent.com/poolfire/pythontestproj/master/flaskapp.service
+sudo wget -O /etc/systemd/system/flaskapp.service https://raw.githubusercontent.com/poolfire/pythontestproj/master/flaskapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable flaskapp
 sudo systemctl start flaskapp
